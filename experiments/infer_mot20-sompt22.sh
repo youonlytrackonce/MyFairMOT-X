c1fddf760ee6b59f2138c9d028b84a087116dd67
youonlytrackonce <f_emre87@hotmail.com>
2023-03-12 13:44:22 +0300

    new scripts added

--- experiments/infer_mot20-sompt22.sh
+++ experiments/infer_mot20-sompt22.sh
@@ -1,6 +1,6 @@
 python3 -W ignore ./src/demo.py mot \
-	    --load_model /home/fatih/phd/FairCenterMOT/exp/mot/mot20-sompt22/logs_2023-02-03-16-48-fairmotx-mot20-sompt22/model_last.pth \
+	    --load_model /home/fatih/phd/FairCenterMOT/exp/mot/data_all/logs_2023-03-07-12-15/model_last.pth \
 	        --input_video /home/fatih/phd/FairCenterMOT/src/data/SOMPT22/images/test/SOMPT22-09/img1 \
 		    --reid_dim 128 --yolo_depth 0.67 --yolo_width 0.75 --reid_cls_id 0 \
-		        --data_cfg /home/fatih/phd/FairCenterMOT/src/lib/cfg/mot20-sompt22.json \
+		        --data_cfg /home/fatih/phd/FairCenterMOT/src/lib/cfg/data_all.json \
 			    --gpus 0
--- experiments/train_data_all.sh
+++ experiments/train_data_all.sh
@@ -2,7 +2,7 @@ python ./src/train.py mot \
 --exp_id data_all --yolo_depth 0.67 --yolo_width 0.75 \
 --lr 1e-3 --lr_step 2 \
 --reid_dim 128 --augment --mosaic \
---load_model /home/fatih/phd/FairCenterMOT/models/yolox_s.pth \
+--load_model /home/fatih/phd/FairCenterMOT/exp/mot/data_all/logs_2023-03-07-12-15/model_last.pth \
 --batch_size 12 --gpus 0 \
 --data_cfg /home/fatih/phd/FairCenterMOT/src/lib/cfg/data_all.json \
---reid_cls_id 0 --num_epochs 30 --num_workers 8
+--reid_cls_id 0 --num_epochs 150 --num_workers 8 --resume --start_epoch 31
