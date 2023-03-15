4c471a7ec2c25fe89bcb361050204e7ac658f879
youonlytrackonce <f_emre87@hotmail.com>
2023-03-15 17:36:17 +0300

    train.py, data_all.train etc modified

--- experiments/infer_mot20-sompt22.sh
+++ experiments/infer_mot20-sompt22.sh
@@ -1,5 +1,5 @@
 python3 -W ignore ./src/demo.py mot \
-	    --load_model /home/fatih/phd/FairCenterMOT/exp/mot/data_all/logs_2023-03-07-12-15/model_last.pth \
+	    --load_model /home/fatih/phd/FairCenterMOT/exp/mot/data_all/model_last.pth \
 	        --input_video /home/fatih/phd/FairCenterMOT/src/data/SOMPT22/images/test/SOMPT22-09/img1 \
 		    --reid_dim 128 --yolo_depth 0.67 --yolo_width 0.75 --reid_cls_id 0 \
 		        --data_cfg /home/fatih/phd/FairCenterMOT/src/lib/cfg/data_all.json \
